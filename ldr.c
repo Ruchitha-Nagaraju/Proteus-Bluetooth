cf806739f7be05666f15df0a124bffaccd38a8af
Ruchitha-Nagaraju <119487338+Ruchitha-Nagaraju@users.noreply.github.com>
2024-03-16 22:37:13 +0530

    Update ldr.c

--- ldr.c
+++ ldr.c
@@ -1,1 +1,21 @@
-#include<pic16f877a.h>#define ldr RB0#define led RC0void main(){TRISB=0XFF;TRISC=0X00;led=0;while(1){if(ldr==0){led=1;}if(ldr==1){led=0;}}}
+#include<pic16f877a.h>
+#define ldr RB0
+#define led RC0
+void main()
+{
+TRISB=0XFF;
+TRISC=0X00;
+led=0;
+while(1)
+{
+if(ldr==0)
+{
+led=1;
+}
+if(ldr==1)
+{
+led=0;
+}
+
+}
+}
